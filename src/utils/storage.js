7e09c634ae7527a3190cee0e05acbaffb05c5071
vjcoderai <onlinekirrana@gmail.com>
2026-05-27 21:22:11 +0530

    Change FileSystem import to legacy version

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,5 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import * as FileSystem from 'expo-file-system';
+import * as FileSystem from 'expo-file-system/legacy';
 
 const DOCS_KEY = 'safescan_v1_docs';
 export const PDF_DIR = FileSystem.documentDirectory + 'safescan_pdfs/';
